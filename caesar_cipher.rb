6716f4a2c6cfe3a21d8761ac1cfa9a68b2502a8b
Marcus Macauley <50225950+macauleydev@users.noreply.github.com>
2023-10-09 10:18:15 -0400

    Refactor

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,36 +1,25 @@
+def caesar_cipher(string, shift)
+  code_A = 65
+  code_Z = 90
+  code_a = 97
+  code_z = 122
 
-
-def caesar_cipher(string, shift_factor)
-code_A = 65
-code_Z = 90
-code_a = 97
-code_z = 122
-  # Convert characters to numbers (codes)
-  numbers = string.codepoints
-  # Filter for letters
-  # Modify those numbers according to shift_factor, including:
-  # Wrap around z to a and Z to A.
-
-
-  ciphertext_array = numbers.map do |n|
-    if (code_a..code_z).include?(n)
-      shift_and_wrap(n, shift_factor, code_a, 26).chr
-    elsif (code_A..code_Z).include?(n)
-      shift_and_wrap(n, shift_factor, code_A, 26).chr
+  ciphertext = ''
+  string.each_codepoint do |code|
+    if (code_A..code_Z).include?(code)
+      ciphertext += shift_and_wrap(code, shift, code_A, 26).chr
+    elsif (code_a..code_z).include?(code)
+      ciphertext += shift_and_wrap(code, shift, code_a, 26).chr
     else
-      n.chr
+      ciphertext += code.chr
     end
   end
 
-  ciphertext = ciphertext_array.join
-
-  # Convert those numbers back to letters
-  # Return the encrypted string
   ciphertext
 end
 
-def shift_and_wrap(number, shift_amount, offset = 0, wrap_length = 26)
-  offset + ((-offset + number + shift_amount) % wrap_length)
+def shift_and_wrap(number, shift, offset = 0, wrap_interval = 26)
+  offset + ((-offset + number + shift) % wrap_interval)
 end
 
 # Test:
