03a8aa6d40ea998ac7dfc8c715e653a72195e199
Marcus Macauley <50225950+macauleydev@users.noreply.github.com>
2023-10-10 16:20:42 -0400

    Tidy

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,29 +1,3 @@
-# Assignment:
-#   Implement a method #stock_picker
-#     that takes in an array of stock prices,
-#       one for each hypothetical day.
-#   It should return a pair of days
-#     representing the best day to buy and the best day to sell.
-#     Days start at 0.
-# Example:
-#   stock_picker([17,3,6,9,15,8,6,1,10])
-#   => [1,4]  # for a profit of $15 - $3 == $12
-# Quick Tips:
-#   You need to buy before you can sell
-#   Pay attention to edge cases
-#     like when the lowest day is the last day
-#     or the highest day is the first day.
-#
-# Solution 1:
-#   Iterate forward through the array:
-#   For each possible buying day:
-#     Find max possible selling day
-#     Calculate the profit
-#     Store those values in a hash (profit => [buying day, selling day])
-#   Search the resulting array for the entry with the greatest max profit.
-#   (If there are multiple results, use the first.)
-#   Return the result as an array: [buying day, selling day]
-
 def stock_picker(daily_prices)
   best_profit_per_buy_day = {}
   daily_prices.each_with_index do |price, buy_day|
